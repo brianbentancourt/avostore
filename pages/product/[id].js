5aaaee59cfe395885c36c31b0f6fe01f14df309b
Brian Bentancourt <brianbentancourt9@gmail.com>
2021-10-12 19:03:44 -0300

    first pages

---
+++ pages/product/[id].js
@@ -0,0 +1,16 @@
+import React from "react"
+import { useRouter } from "next/router"
+
+const ProductItem = () => {
+    const {
+        query: { id }
+    } = useRouter()
+
+    return (
+        <div>
+            <h1>pagina del producto: {id}</h1>
+        </div>
+    )
+}
+
+export default ProductItem
