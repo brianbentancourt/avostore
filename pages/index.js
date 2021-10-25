e1a50bfddcdb701b9a0780438f408bddf46ab75f
Brian Bentancourt <brianbentancourt9@gmail.com>
2021-10-25 15:35:27 -0300

    fetch data from id

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import NavBar from '../components/NavBar'
+import Link from "next/link"
 
 const Home = () => {
+    const [productList, setProductList] = useState([])
+
+    useEffect(() => {
+        window.fetch('/api/avo')
+            .then(response => response.json())
+            .then(({ data, length }) => setProductList(data))
+    }, [])
 
     return (
         <div>
+            <NavBar />
             <h1>Hola mundo!</h1>
+            {
+                productList.map(product =>
+                    <div key={product.id}>
+
+                        <Link href={`/product/${product.id}`}>
+                            <a>{product.name}</a>
+                        </Link>
+                    </div>
+                )
+            }
         </div>
     )
 }
--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { useRouter } from "next/router"
 
 const ProductItem = () => {
@@ -6,9 +6,24 @@ const ProductItem = () => {
         query: { id }
     } = useRouter()
 
+    const [productDetail, setProductDetail] = useState(null)
+
+    useEffect(() => {
+        window.fetch('/api/avo/' + id)
+            .then(response => response.json())
+            .then((data) => setProductDetail(data))
+    }, [])
+
     return (
         <div>
             <h1>pagina del producto: {id}</h1>
+            {
+                productDetail &&
+                <>
+                    <p>{productDetail.attributes.description}</p>
+                </>
+            }
+
         </div>
     )
 }
