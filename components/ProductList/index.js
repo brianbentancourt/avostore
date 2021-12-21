94131dfba0ab55e2b0ae3ba212a7bde3876e6a62
Brian Bentancourt <brianbentancourt9@gmail.com>
2021-12-21 12:45:20 -0300

    image optimization and lazy loading

--- components/ProductList/index.js
+++ components/ProductList/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Card } from 'semantic-ui-react'
 import Link from 'next/link'
-
+import Image from 'next/image'
 
 const mapProductsToCards = (products = []) =>
     products.map(({ name, id, price, image }) => (
@@ -9,7 +9,7 @@ const mapProductsToCards = (products = []) =>
             <Card
                 as="a"
                 header={name}
-                image={image}
+                image={<Image src={image} width={333} height={333} />}
                 meta={<Card.Meta style={{ color: 'dimgray' }}>{price}</Card.Meta>}
             />
         </Link>
